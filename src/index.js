e8e4dade6c026de995bd10d058cb277157363586
Mike <dev.mhowells@gmail.com>
2022-03-09 19:35:01 +0000

    second recovered version after another break

---
+++ src/index.js
@@ -0,0 +1,132 @@
+import "./style.css";
+import { projectPage } from "./project-page";
+
+// ------------------------DOM----------------------------------
+
+const main = document.getElementById("main");
+const rightSide = document.getElementById("right-side");
+const addProjectBtn = document.getElementById("add-project"); // In Nav
+
+const createBtn = document.getElementById("create"); // In modal
+
+// CLEAR FUNCTIONS -------------------------------------------------------------
+
+const clearPage = function (element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
+
+// ----------------------Projects GRID -------------------------------
+const projectsGrid = document.createElement("div");
+const createProjectsGrid = function () {
+  clearPage(rightSide);
+  projectsGrid.classList.add("projects-grid");
+  rightSide.appendChild(projectsGrid);
+};
+
+export const projectClasses = [];
+export let currentProject = 0; // TRACKS CURRENT PROJECT IN ADD TASK BUTTON MODIFIED AS BUTTON CREATED, STORED BY IT.
+
+// -------------------- TASKS GRID --------------------------------
+
+// projectPage(rightSide);
+
+// TARGET MODAL -----------
+
+const modal_container = document.getElementById("modal-container");
+
+//-------------------------FUNCTIONS-------------------------------
+
+// MODAL FUNCTIONS --------------------------------------------------------------
+const openModal = function () {
+  modal_container.style.display = "flex";
+};
+
+const closeModal = function () {
+  modal_container.style.display = "none";
+};
+
+// CLICK EVENTS -------------------------------------------------------------
+
+addProjectBtn.addEventListener("click", openModal);
+
+createBtn.addEventListener("click", function () {
+  closeModal();
+  createProjectsGrid();
+  createProjectSummary();
+});
+
+// POPULATE PROJECTS GRID -------------------------------------------------
+
+const popProjectectsGrid = function () {
+  for (let i = 0; i < projectClasses.length; i++) {
+    console.log(projectClasses[i]);
+
+    const projectSummary = document.createElement("div");
+    projectSummary.classList.add("project-summary");
+    projectsGrid.appendChild(projectSummary);
+
+    const projectTitleDisplay = document.createElement("h3");
+    projectSummary.appendChild(projectTitleDisplay);
+    projectTitleDisplay.textContent = projectClasses[i].projectTitle;
+
+    const projectDescriptionDisplay = document.createElement("p");
+    projectSummary.appendChild(projectDescriptionDisplay);
+    projectDescriptionDisplay.textContent =
+      projectClasses[i].projectDescription;
+
+    const addTask = document.createElement("button");
+    addTask.textContent = "Add Tasks";
+    projectSummary.appendChild(addTask);
+
+    addTask.addEventListener("click", function () {
+      currentProject = i;
+      console.log(currentProject);
+      console.log(projectClasses[i]);
+      clearPage(rightSide);
+      projectPage(rightSide);
+    });
+  }
+};
+
+// GET INPUT --------------------------------------------------------------
+
+const getTitle = function () {
+  const projectTitleInput = document.getElementById("project-title").value;
+  return projectTitleInput;
+};
+
+const getDescription = function () {
+  const projectDescriptionInput = document.getElementById("description").value;
+  return projectDescriptionInput;
+};
+
+const createProjectSummary = function () {
+  // CLEAR RIGHT SIDE
+  clearPage(projectsGrid);
+  // CREATE NEW CLASS
+  const newProject = new Project(getTitle(), getDescription());
+  // ADD TO CLASS LIST
+  projectClasses.push(newProject);
+  // POPULATE PROJECTS GRID
+  popProjectectsGrid();
+};
+
+class Project {
+  constructor(projectTitle, projectDescription, dueDate, priority) {
+    this.projectTitle = projectTitle;
+    this.projectDescription = projectDescription;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.todos = [];
+  }
+}
+
+const allProjects = document.getElementById("all-projects");
+allProjects.addEventListener("click", function () {
+  console.log("test");
+  createProjectsGrid();
+  clearPage(projectsGrid);
+  popProjectectsGrid();
+});
---
+++ src/project-page.js
@@ -0,0 +1,50 @@
+import "./style.css";
+import { projectClasses } from ".";
+import { currentProject } from ".";
+
+export const projectPage = function (parent) {
+  const eachPrjGrid = document.createElement("div");
+  eachPrjGrid.classList.add("each-prj-grid");
+  parent.appendChild(eachPrjGrid);
+
+  const prjGridDate = document.createElement("div");
+  prjGridDate.classList.add("prj-grid-date");
+  eachPrjGrid.appendChild(prjGridDate);
+
+  const prjGridTitle = document.createElement("div");
+  prjGridTitle.classList.add("prj-grid-title");
+  eachPrjGrid.appendChild(prjGridTitle);
+
+  const prjGridPriority = document.createElement("div");
+  prjGridPriority.classList.add("prj-grid-priority");
+  eachPrjGrid.appendChild(prjGridPriority);
+
+  const prjGridDesc = document.createElement("div");
+  prjGridDesc.classList.add("prj-grid-description");
+  eachPrjGrid.appendChild(prjGridDesc);
+
+  prjGridDate.textContent = "Date";
+  prjGridTitle.textContent = projectClasses[currentProject].projectTitle;
+  prjGridPriority.textContent = "Priority";
+  prjGridDesc.textContent = projectClasses[currentProject].projectDescription;
+
+  const prjGridTasks = document.createElement("div"); // NEW GRID
+  prjGridTasks.classList.add("prj-grid-tasks");
+  eachPrjGrid.appendChild(prjGridTasks);
+
+  const prjtasksTitle = document.createElement("div");
+  prjtasksTitle.classList.add("prj-tasks-title");
+  prjGridTasks.appendChild(prjtasksTitle);
+
+  const prjtasks1 = document.createElement("div");
+  prjtasks1.classList.add("prj-task");
+  prjGridTasks.appendChild(prjtasks1);
+
+  const prjtasks2 = document.createElement("div");
+  prjtasks2.classList.add("prj-task");
+  prjGridTasks.appendChild(prjtasks2);
+
+  prjtasksTitle.textContent = "TASKS:";
+  prjtasks1.textContent = "task 1";
+  prjtasks2.textContent = "task 2";
+};
